7452662ccd5a6a8088b45409c27fef132258bc1c
Florian Bieck <florian@bieck.com>
2020-06-03 17:58:41 +0200

    Aufgabe 1

--- app/src/main/java/com/fbieck/todo/data/TodoRepository.kt
+++ app/src/main/java/com/fbieck/todo/data/TodoRepository.kt
@@ -2,46 +2,47 @@ package com.fbieck.todo.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import com.fbieck.todo.data.db.TodoDao
+import androidx.lifecycle.MutableLiveData
 import com.fbieck.todo.data.db.TodoDatabase
-import com.fbieck.todo.data.db.TodoRecord
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class TodoRepository(application: Application) {
 
-    private val todoDao: TodoDao
-    private val allTodos: LiveData<List<TodoRecord>>
+    private val allTodos: LiveData<List<String>>
 
     init {
         val database = TodoDatabase.getInstance(application.applicationContext)
-        todoDao = database!!.todoDao()
-        allTodos = todoDao.getAllTodoList()
+
+        val placeholder: MutableLiveData<List<String>> by lazy {
+            MutableLiveData<List<String>>()
+        }
+        allTodos = placeholder
     }
 
-    fun saveTodo(todo: TodoRecord) = runBlocking {
+    fun saveTodo(todo: String) = runBlocking {
         this.launch(Dispatchers.IO) {
-            todoDao.saveTodo(todo)
+
         }
     }
 
-    fun updateTodo(todo: TodoRecord) = runBlocking {
+    fun updateTodo(todo: String) = runBlocking {
         this.launch(Dispatchers.IO) {
-            todoDao.updateTodo(todo)
+
         }
     }
 
 
-    fun deleteTodo(todo: TodoRecord) {
+    fun deleteTodo(todo: String) {
         runBlocking {
             this.launch(Dispatchers.IO) {
-                todoDao.deleteTodo(todo)
+
             }
         }
     }
 
-    fun getAllTodoList(): LiveData<List<TodoRecord>> {
+    fun getAllTodoList(): LiveData<List<String>> {
         return allTodos
     }
 }
--- app/src/main/java/com/fbieck/todo/data/db/TodoDatabase.kt
+++ app/src/main/java/com/fbieck/todo/data/db/TodoDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [TodoRecord::class], version = 1, exportSchema = false)
+@Database(entities = [String::class], version = 1, exportSchema = false)
 abstract class TodoDatabase : RoomDatabase() {
 
     abstract fun todoDao(): TodoDao
